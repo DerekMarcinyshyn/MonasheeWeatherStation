28d04e697457342f3560192c6e9d248ca357dbaa
DerekMarcinyshyn <derek@monasheemountainmultimedia.com>
2013-07-12 23:24:46 -0700

    add BMP085 barometer/temperature sensor

--- Program.cs
+++ Program.cs
@@ -1,44 +1,63 @@
 ﻿using System;
-using System.Text;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using Microsoft.SPOT;
 using Microsoft.SPOT.Hardware;
 using SecretLabs.NETMF.Hardware;
-using SecretLabs.NETMF.Hardware.NetduinoPlus;
-using CW.NETMF;
+using SecretLabs.NETMF.Hardware.Netduino;
+using CW.NETMF; // temp/humidity drivers
+using Toolbox.NETMF.Hardware;
 
 namespace MonasheeWeatherStation
 {
     public class Program
     {
         static Dht22Sensor temphumidity = new Dht22Sensor(Pins.GPIO_PIN_D0, Pins.GPIO_PIN_D1, PullUpResistor.Internal);
+        static BMP085 barometer = new BMP085(0x77, BMP085.DeviceMode.UltraHighResolution);
 
-        /**
-         * Main program
-         */
+        /// <summary>
+        /// Main program
+        /// </summary>
         public static void Main()
         {
             // let the Netduino fire up first
-            Thread.Sleep(3000);
+            Thread.Sleep(500);
 
             /**** MAIN LOOP ****/
             while (true)
             {
 
-                temperatureHumidity();
+                barometerTemperature();
+
+                //temperatureHumidity();
                 
                 // Loop every X seconds?
-                Thread.Sleep(3000);
+                Thread.Sleep(10000);
                 
             }
             /**** END MAIN LOOP ****/
         }
 
-        /**
-         * Temperature and Humidity
-         */
+        /// <summary>
+        /// Barometer and Temperture
+        /// </summary>
+        private static void barometerTemperature()
+        {
+            // compensate for elevation
+            int altitude = 500;
+            double altimeter = (float)101325 * System.Math.Pow(((288 - 0.0065 * altitude) / 288), 5.256);
+            double pressureASL = (101325 + barometer.Pascal) - altimeter;
+
+            Debug.Print("Pascal: " + barometer.Pascal);
+            Debug.Print("kPa: " + pressureASL);
+            Debug.Print("Mg: " + barometer.InchesMercury.ToString("F2"));
+            Debug.Print("Temp: " + barometer.Celsius.ToString("F2"));
+        }
+
+        /// <summary>
+        /// Temperature and Humidity
+        /// </summary>
         private static void temperatureHumidity()
         {
             if (temphumidity.Read())
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MonasheeWeatherStation")]
+[assembly: AssemblyTitle("Monashee Weather Station")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MonasheeWeatherStation")]
+[assembly: AssemblyCompany("Monashee Mountain Multimedia")]
+[assembly: AssemblyProduct("Monashee Weather Station")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
